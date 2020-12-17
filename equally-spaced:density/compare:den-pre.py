dd82c7cd280f74fac69ed2331b1880d4a64ff734
S.Naito <na.shota.1139@gmail.com>
2020-12-17 21:35:33 +0900

    Modified

--- equally-spaced:density/compare:den-pre.py
+++ equally-spaced:density/compare:den-pre.py
@@ -13,24 +13,27 @@ y_all = y_all.tolist()
 x_all.insert(0,0)
 y_all.insert(0,0)
 
+for i in range(101):
+    density = 0.01 * i
+    if density == 0:
+        density = 0
+        pressure = 0
+    else:
+        volume = 2048/density
+        length = math.pow(volume, 1/3)
+        distance = math.sqrt(2)*(length/16)
+        pressure = (1/(volume))*(2048) 
+    with open("compare:den-pre.dat", "a") as f:
+        f.write("{} {} {}\n".format(density, pressure, y_all[i]))
 
-for i in range(501):
-    length = 6 + i/500
-    length = float(format(length, '.3f'))
-    volume = length**3
-    density = 2048/volume
+
+for i in range(1,1401):
+    density = 1.0 + 0.01*i
+    volume = 2048/density
+    length = math.pow(volume, 1/3)
     distance = math.sqrt(2)*(length/16)
     dis_pow = abs((48*distance**(-12)-24*distance**(-6))*12*2048/2048/2)
     pressure = (1/(volume))*(2048+2048*dis_pow/3) 
     with open("compare:den-pre.dat", "a") as f:
-        f.write("{} {} {}\n".format(density, pressure, y_all[i]))
+        f.write("{} {} {}\n".format(density, pressure, y_all[100+i]))
 
-for i in range(501):
-    length = 6 + i/500
-    length = float(format(length, '.3f'))
-    volume = length**3
-    density = 2048/volume
-    distance = math.sqrt(2)*(length/16)
-    pressure = (1/(volume))*(2048) 
-    with open("compare:den-pre.dat", "a") as f:
-        f.write("{} {} {}\n".format(density, pressure, y_all[i]))
